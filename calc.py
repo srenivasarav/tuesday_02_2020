8ea1663c9b801337480fd82175ad4e71d3ecbb32
Srinivasa Rao B <srenivas35@gmail.com>
2020-03-05 18:16:30 +0530

    wiki data

--- calc.py
+++ calc.py
@@ -5,6 +5,12 @@ def substract(x,y):
 def divide(x,y):
     (x*y)
 def multiply(x,y):
-    pass
+    passb
 def square(x):
-    pass
+    passs
+
+qqqqqqqqqqqqqqqqqq
+aaaaaaaaaaaaaaaaaa
+000000000000000000
+-----------------
+compendiums of information, usually of a specific type, compiled in a book for ease of reference. That is, the information is intended to be quickly found when needed.
